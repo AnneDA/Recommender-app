9392efb177d9536debe4b4f847c6f8e7a729f050
AnneDA <86477496+AnneDA@users.noreply.github.com>
2021-06-25 20:50:56 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,7 @@
-mkdir -p ~/.streamlit/
+mkdir -p ~/.streamlit
 
-echo "\
-[general]\n\
-email = \"Annekathrin.Bedard@web.de\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
